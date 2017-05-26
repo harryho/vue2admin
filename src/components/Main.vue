34bac4c11c4ee0161c846c61a11e82d0b04f4d62
Harry Ho <harry.ho_long@yahoo.com>
2017-05-27 03:16:40 +1000

    Add profile dropdown card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -110,13 +110,48 @@
               </ul>
             </li>
             <!-- User Account Menu -->
-            <li class="dropdown user user-menu">
+            <li class="dropdown user user-menu" @click="topBarToggleMenu">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                 <!-- The user image in the navbar-->
                 <img v-bind:src="demo.avatar" class="user-image" alt="User Image">
                 <!-- hidden-xs hides the username on small devices so only the image appears. -->
                 <span class="hidden-xs">{{ demo.displayName }}</span>
               </a>
+              <ul class="dropdown-menu">
+                <!-- User image -->
+                <li class="user-header">
+                  <img v-bind:src="demo.avatar"  class="img-circle" alt="User Image">
+
+                  <p>
+                    Harry Ho - Solution Architect
+                    <small>Member since Nov. 2010</small>
+                  </p>
+                </li>
+                <!-- Menu Body -->
+                <li class="user-body">
+                  <div class="row">
+                    <div class="col-xs-4 text-center">
+                      <a href="#">Followers</a>
+                    </div>
+                    <div class="col-xs-4 text-center">
+                      <a href="#">Sales</a>
+                    </div>
+                    <div class="col-xs-4 text-center">
+                      <a href="#">Friends</a>
+                    </div>
+                  </div>
+                  <!-- /.row -->
+                </li>
+                <!-- Menu Footer-->
+                <li class="user-footer">
+                  <div class="pull-left">
+                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                  </div>
+                  <div class="pull-right">
+                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  </div>
+                </li>
+            </ul>
             </li>
             <!-- Control Sidebar Toggle Button -->
             <li>
@@ -133,7 +168,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel">
           <div class="pull-left image">
-              <img v-bind:src="demo.avatar" class="user-image user-menu" alt="User Image">
+              <img v-bind:src="demo.avatar" class="img-circle" alt="User Image">
           </div>
           <div class="pull-left info">
             <div>
